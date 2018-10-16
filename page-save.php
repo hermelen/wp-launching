dec0b8b9a8a1c55a3dc7b3b7baa0767417178214
PRUNIERES <aurelie.prunieres@gmail.com>
2018-10-16 14:46:12 +0200

    html first part done

--- page-save.php
+++ page-save.php
@@ -17,13 +17,24 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-            <?php if ( is_home() || is_front_page() ) :
-                get_template_part( 'template-parts/content', 'home' );
-            ?>
+<h2>Yep</h2>
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
-            <?php endif ?>
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
---
+++ template-parts/content-home.php
@@ -0,0 +1,13 @@
+<h1>WE ARE LAUNCHING IN</h1>
+<h2>5</h2>
+<h2>DAYS</h2>
+<p>
+    Codi- High quality Bootstrap HTML5Coming Soon Landing Page Template
+</p>
+<p>
+    Comes with fully responsive layout, Cool features, and Clean design.
+</p>
+
+<p>
+    Template Handcrafted By Codi'n Camp
+</p>
